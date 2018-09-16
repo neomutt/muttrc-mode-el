bb385ce2ed260b0b213ec177faac0b088781a31e
Nicholas D Steeves <nsteeves@gmail.com>
2018-09-15 21:56:53 -0400

    Use conventional header style for Emacs libraries

--- muttrc-mode.el
+++ muttrc-mode.el
@@ -1,24 +1,24 @@
 ;;; muttrc-mode.el --- Major mode to edit muttrc under Emacs
 
-;;; Copyright (C) 2000, 2001, 2002 Laurent Pelecq
-;;; Copyright (C) 2009 Kumar Appaiah
-;;;
-;;; Authors: Laurent Pelecq <laurent.pelecq@soleil.org>
-;;;          Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
+;; Copyright (C) 2000, 2001, 2002 Laurent Pelecq
+;; Copyright (C) 2009 Kumar Appaiah
+;;
+;; Author: Laurent Pelecq <laurent.pelecq@soleil.org>
+;;         Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
 
-;;; This program is free software; you can redistribute it and/or modify
-;;; it under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation; either version 2, or (at your option)
-;;; any later version.
-;;;
-;;; This program is distributed in the hope that it will be useful,
-;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;; GNU General Public License for more details.
-;;;
-;;; You should have received a copy of the GNU General Public License
-;;; along with this program; if not, write to the Free Software
-;;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
 ;;; Supported Emacs:
 ;;; ================
