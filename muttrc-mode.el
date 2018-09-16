75e867e36bd971c679ebcaf72ab1c6d9a437aa10
Nicholas D Steeves <nsteeves@gmail.com>
2018-09-15 21:57:37 -0400

    Add Version and Homepage to header

--- muttrc-mode.el
+++ muttrc-mode.el
@@ -5,6 +5,8 @@
 ;;
 ;; Author: Laurent Pelecq <laurent.pelecq@soleil.org>
 ;;         Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
+;; Version: 1.2
+;; Homepage: https://github.com/neomutt/muttrc-mode-el
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
